1a54c5c7805b26f653f2756052fc3053ba2b5824
Steven Franklin <steve11sjf@gmail.com>
2021-05-03 03:53:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var bodyparser = require('body-parser');
+var fs = require("fs");
+var path = require('path');
+app.use(bodyparser());
+
+app.listen(9007, () => console.log('Listening on port 9007!'));
+__discord_link = "https://discord.gg/HxxNybCgM4"
+
+// entry page
+app.get('/',function(req, res) {
+  res.redirect('/Home/');
+});
+
+app.get('/Home/', function(req, res) {
+	res.sendFile(__dirname + '/client/welcome.html');
+});
+
+app.get('/Standings/', function(req, res) {
+	res.sendFile(__dirname + '/client/standings.html');
+});
+
+app.get('/Schedule/', function(req, res) {
+	res.sendFile(__dirname + '/client/schedule.html');
+});
+
+app.get('/Stats/', function(req, res) {
+	res.sendFile(__dirname + '/client/stats.html');
+});
+
+// redirects user to discord link
+app.get('/Discord/', function(req, res) {
+	res.redirect(__discord_link);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/client/404.html');
+});
