2d8379f28abeba879eb919be455094bbad9c485c
Steven Franklin <steve11sjf@gmail.com>
2021-05-03 21:29:21 -0500

    Updated to work with environment variables

---
+++ auth.js
@@ -0,0 +1,20 @@
+const {GoogleAuth, JWT} = require('google-auth-library');
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+
+function authorize() {
+    return new Promise(resolve => {
+        const authFactory = new GoogleAuth();
+        const jwtClient = new JWT({
+            email: process.env.GOOGLE_CLIENT_EMAIL,
+            key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+            scopes: SCOPES
+        });
+
+        jwtClient.authorize(() => resolve(jwtClient));
+    });
+}
+
+module.exports = {
+    authorize,
+}
