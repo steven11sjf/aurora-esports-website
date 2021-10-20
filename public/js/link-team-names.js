749f2e91d33e8200facaea01858449033a136325
Steven Franklin <steve11sjf@gmail.com>
2021-10-20 09:31:51 -0500

    bug fixes for 2.0
    - navbar can handle invalid season name
    - tweaked link-team-names.js to provide comment clarity
    - updated blogpost for 2.0 to reflect completed bug fixes

--- public/js/link-team-names.js
+++ public/js/link-team-names.js
@@ -18,15 +18,15 @@ function addLinks()
 	//iterate the array
     $.each(links_dict,
         function() {
+			// grab word/link from 'this' before it is changed
             var searchWord = this.word;
             var link = this.link;
-			console.log(searchWord,link);
+			// jquery to get all instances of searchWord in a p object
             $('p:contains("' + searchWord + '")').each(function() {
-				
+				// replace first instance of searchWord with a link
                 var newHtml = $(this).html().replace(searchWord, 
                     '<a class="auto-plink" href="'+link+'">' + searchWord + '</a>');
                 $(this).html(newHtml);
-				console.log(newHtml);
             });
         }
     );
