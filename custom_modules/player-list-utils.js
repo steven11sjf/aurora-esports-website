6c8151fb99c13b9826f23e71299c08bd3b212125
Steven Franklin <steve11sjf@gmail.com>
2022-03-01 13:41:00 -0600

    temporary store while I fix rest of site

---
+++ custom_modules/player-list-utils.js
@@ -0,0 +1,7 @@
+// ==========
+// Module run on build that combines all gopherwatch player profiles into one dataset and caches into the main site sheet
+// this file is part of the GWL website which is hosted at www.gopherwatchleague.com
+// git repository at https://github.com/hyperbola0/gopherwatch-league-site
+// see file ./LICENSE for full license details.
+// (c) 2022 Steven Franklin. 
+// ==========
